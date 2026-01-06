56082938dbf71e71e0a5a88a8457fba75d3afd48
Anol Sarker Dipto <anol.dipto@reserveitbd.com>
2026-01-06 15:32:28 +0600

    remove Squash Updates

--- components/HockeySquashNewsGrid.tsx
+++ components/HockeySquashNewsGrid.tsx
@@ -154,7 +154,6 @@ const HockeySquashNewsGrid: React.FC = () => {
           <h1 className="text-3xl md:text-4xl font-bold mb-2">
             News & Announcements
           </h1>
-          <p className="text-gray-700">Hockey & Squash Updates</p>
         </header>
 
         {/* News Grid */}
